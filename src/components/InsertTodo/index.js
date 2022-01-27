002d9309cdc5286970ca3778a8c8d83d9d4a8b3e
ImNotAOwl <siegel.matthieu@ymail.com>
2022-01-27 14:18:25 +0100

    create a components to add a todo

---
+++ src/components/InsertTodo/index.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import { useRef } from 'react';
+import './index.css';
+
+
+const InsertTodo = ({setTodos}) => {
+
+    const task_desc = useRef();
+    const attribute_to = useRef();
+    const statusTask = useRef();
+    console.log(task_desc);
+
+    const addTodo = async (e) => {
+        e.preventDefault();  // annule le refresh par défaut au click du submit
+        const result = await axios.post('http://localhost:8000/api/todos/', {
+            task_desc: task_desc.current.value,
+            attribute_to: attribute_to.current.value
+        })
+         
+        setTodos(prevstate => [...prevstate, result.data.result]);
+        console.log(statusTask);
+
+        statusTask.current.innerText = result.data.success;
+        task_desc.current.value = "";
+        task_desc.current.focus();
+        attribute_to.current.value = "";
+    }
+
+    const cleanStatus = () => {
+        statusTask.current.innerText = "";
+    }
+
+    return (
+        <>
+            <form className="form_insert_todo">
+                <input type="text" ref={task_desc} onChange={cleanStatus}/>
+                <input type="text" ref={attribute_to} onChange={cleanStatus}/>
+                <input type="submit" value="Insert" onClick={(e) => addTodo(e)} />
+                <p ref={statusTask}></p>
+            </form>
+        </>
+    )
+
+};
+
+export default InsertTodo;
